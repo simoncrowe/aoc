baf1559a89d649c1965634f6f2488ff95c7aae2b
Simon Crowe <simon.r.crowe@protonmail.com>
2022-12-14 23:47:21 +0000

    Add Pos struct, attempt to constrain traversal

--- 2022/rust/day_12/src/main.rs
+++ 2022/rust/day_12/src/main.rs
@@ -16,24 +16,31 @@ pub fn main() {
 
 fn compute_shortest_path_length(input_path: &str) -> io::Result<usize> {
     let grid = Terrain::new(input_path).unwrap();
-    let mut paths: Vec<Vec<(usize, usize)>> = Vec::new();
-    let mut paths_to_dest: Vec<Vec<(usize, usize)>> = Vec::new();
+    let mut paths: Vec<Vec<Pos>> = Vec::new();
+    let mut paths_to_dest: Vec<Vec<Pos>> = Vec::new();
     paths.push(vec![grid.start]);
+    let mut traversed: HashSet<Pos> = HashSet::new();
     let mut depth = 0;
     loop {
         println!("\nChecking depth {}", depth);
         depth += 1;
 
         let mut fully_traversed = true;
-        let mut new_paths: Vec<Vec<(usize, usize)>> = Vec::new();
+        let mut new_paths: Vec<Vec<Pos>> = Vec::new();
         for path in paths.iter_mut().filter(|p| *p.last().unwrap() != grid.end) {
-            let cur_pos = path.last().unwrap();
-            let steps = get_possible_steps(&grid, *cur_pos);
-            let traversed: HashSet<(usize, usize)> = HashSet::from_iter(path.clone().into_iter());
-            let mut steps_iter = steps.into_iter().filter(|pos| !traversed.contains(pos));
+            let path_copy = path.clone();
+            let cur_pos = path_copy.last().unwrap();
+            let possible_steps = get_possible_steps(&grid, *cur_pos);
+            let selected_steps = select_steps(cur_pos, possible_steps, &grid.end);
+            //let traversed_copy: HashSet<Pos> = HashSet::from_iter(path.clone().into_iter());
+            let traversed_copy = traversed.clone();
+            let mut steps_iter = selected_steps
+                .into_iter()
+                .filter(|pos| !traversed_copy.contains(pos));
             match steps_iter.next() {
                 Some(pos) => {
                     path.push(pos);
+                    traversed.insert(pos);
                     if pos == grid.end {
                         paths_to_dest.push(path.clone());
                     } else {
@@ -46,6 +53,7 @@ fn compute_shortest_path_length(input_path: &str) -> io::Result<usize> {
                 //println!("Splitting to new line at {pos:?}");
                 let mut new_path = path.clone();
                 new_path.push(pos);
+                traversed.insert(pos);
                 if pos == grid.end {
                     paths_to_dest.push(new_path);
                 } else {
@@ -61,7 +69,7 @@ fn compute_shortest_path_length(input_path: &str) -> io::Result<usize> {
             Some(len) => {
                 fewest_steps = len - 1;
                 println!("Fewest steps {}", fewest_steps);
-            },
+            }
             None => {}
         }
         if fully_traversed {
@@ -71,59 +79,101 @@ fn compute_shortest_path_length(input_path: &str) -> io::Result<usize> {
     Ok(paths_to_dest.iter().map(|path| path.len()).min().unwrap() - 1)
 }
 
-fn get_possible_steps(grid: &Terrain, pos: (usize, usize)) -> Vec<(usize, usize)> {
-    let cur_val = grid.get_elevation(pos.0, pos.1).unwrap();
-    let mut dirs: Vec<(usize, usize)> = Vec::new();
-    if pos.1 <= grid.height {
-        match grid.get_elevation(pos.0, pos.1 + 1) {
+fn select_steps(current: &Pos, potential: Vec<Pos>, target: &Pos) -> Vec<Pos> {
+    let perfect_direction: Vec<Pos> = potential.clone().into_iter().filter(|p| towards(current, p, target, 1)).collect();
+    if perfect_direction.len() > 0 {
+        return perfect_direction;
+    } 
+    potential.clone().into_iter().filter(|p| towards(current, p, target, 2)).collect()
+}
+
+fn towards(current: &Pos, potential: &Pos, target: &Pos, tolerance: usize) -> bool {
+    let mut away_count = 0;
+    if current.x < target.x && potential.x < current.x {
+        away_count += 1; 
+    }
+    if current.y < target.y && potential.y < current.y {
+        away_count += 1; 
+    }
+    if current.x > target.x && potential.x > current.x {
+        away_count += 1; 
+    }
+    if current.y > target.y && potential.y > current.y {
+        away_count += 1; 
+    }
+    away_count < tolerance 
+}
+
+fn get_possible_steps(grid: &Terrain, pos: Pos) -> Vec<Pos> {
+    let cur_val = grid.get_elevation(pos.x, pos.y).unwrap();
+    let mut dirs: Vec<Pos> = Vec::new();
+    if pos.y <= grid.height {
+        match grid.get_elevation(pos.x, pos.y + 1) {
             Some(elevation) => {
                 if cur_val + 1 >= elevation {
-                    dirs.push((pos.0, pos.1 + 1));
+                    dirs.push(Pos {
+                        x: pos.x,
+                        y: pos.y + 1,
+                    });
                 }
             }
             None => {}
         }
     }
-    if pos.0 <= grid.width {
-        match grid.get_elevation(pos.0 + 1, pos.1) {
+    if pos.x <= grid.width {
+        match grid.get_elevation(pos.x + 1, pos.y) {
             Some(elevation) => {
                 if cur_val + 1 >= elevation {
-                    dirs.push((pos.0 + 1, pos.1));
+                    dirs.push(Pos {
+                        x: pos.x + 1,
+                        y: pos.y,
+                    });
                 }
             }
             None => {}
         }
     }
-    if pos.1 != 0 {
-        match grid.get_elevation(pos.0, pos.1 - 1) {
+    if pos.y != 0 {
+        match grid.get_elevation(pos.x, pos.y - 1) {
             Some(elevation) => {
                 if cur_val + 1 >= elevation {
-                    dirs.push((pos.0, pos.1 - 1));
+                    dirs.push(Pos {
+                        x: pos.x,
+                        y: pos.y - 1,
+                    });
                 }
             }
             None => {}
         }
     }
-    if pos.0 != 0 {
-        match grid.get_elevation(pos.0 - 1, pos.1) {
+    if pos.x != 0 {
+        match grid.get_elevation(pos.x - 1, pos.y) {
             Some(elevation) => {
                 if cur_val + 1 >= elevation {
-                    dirs.push((pos.0 - 1, pos.1));
+                    dirs.push(Pos {
+                        x: pos.x - 1,
+                        y: pos.y,
+                    });
                 }
             }
             None => {}
         }
     }
-    //println!("Found {} directions for {}, {}", dirs.len(), pos.0, pos.1);
+    //println!("Found {} directions for {}, {}", dirs.len(), pos.x, pos.y);
     dirs
+}
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
+struct Pos {
+    x: usize,
+    y: usize,
 }
 
 struct Terrain {
     elevations: Vec<u8>,
     width: usize,
     height: usize,
-    start: (usize, usize),
-    end: (usize, usize),
+    start: Pos,
+    end: Pos,
 }
 
 impl Terrain {
@@ -133,8 +183,8 @@ impl Terrain {
         let mut elevations: Vec<u8> = Vec::new();
         let mut width: usize = 0;
         let mut height: usize = 0;
-        let mut start: (usize, usize) = (0, 0);
-        let mut end: (usize, usize) = (0, 0);
+        let mut start = Pos { x: 0, y: 0 };
+        let mut end = Pos { x: 0, y: 0 };
         for (row_idx, line) in lines
             .map(|ln| ln.unwrap())
             .collect::<Vec<String>>()
@@ -147,11 +197,11 @@ impl Terrain {
                 let letter = std::str::from_utf8(&bytes).unwrap();
                 //println!("{}, {}: {} ({})", row_idx, col_idx, ord, letter);
                 if ord == UPPERCASE_S_ORD {
-                    start.0 = col_idx;
-                    start.1 = row_idx;
+                    start.x = col_idx;
+                    start.y = row_idx;
                 } else if ord == UPPERCASE_E_ORD {
-                    end.0 = col_idx;
-                    end.1 = row_idx;
+                    end.x = col_idx;
+                    end.y = row_idx;
                 }
                 elevations.push(height_from_ord(ord));
             }
@@ -193,9 +243,9 @@ mod tests {
     fn test_terain_new() {
         let test_input_path = "/home/sc/git/aoc/2022/input/12_test_input.txt";
         let grid = Terrain::new(test_input_path).unwrap();
-        assert_eq!((5, 2), grid.end);
+        assert_eq!(Pos { x: 5, y: 2 }, grid.end);
         assert_eq!(25, grid.get_elevation(5, 2).unwrap());
-        assert_eq!((0, 4), grid.start);
+        assert_eq!(Pos { x: 0, y: 4 }, grid.start);
         assert_eq!(0, grid.get_elevation(0, 4).unwrap());
     }
 

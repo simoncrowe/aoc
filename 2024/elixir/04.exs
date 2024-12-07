b6aa38177c763d8176423c696bb7aab6e481fa15
Simon Crowe <simon.r.crowe@protonmail.com>
2024-12-07 19:18:33 +0000

    Solve day 4 part 1

--- 2024/elixir/04.exs
+++ 2024/elixir/04.exs
@@ -1,73 +1,67 @@
 defmodule Search do
-  def scan_multi(text_grid, pattern) do 
+  def all_slices(text_grid) do 
     rows = text_grid |> String.trim() |> String.split("\n")
     {width, height} = {rows |> Enum.at(0) |> String.length, rows |> Enum.count}
     IO.puts("Width: #{width}; height: #{height}")
     flat = rows |> Enum.join("") |> String.graphemes
-    flat_reversed = flat |> Enum.reverse
-    [
-      Search.horizontal_scan(flat, pattern, width),
-      Search.horizontal_scan(flat_reversed, pattern, width),
-      Search.vertical_scan(flat, pattern, width, height),
-      Search.vertical_scan(flat_reversed, pattern, width, height),
-      Search.diagonal_scan_left(flat, pattern, width, height),
-      Search.diagonal_scan_left(flat_reversed, pattern, width, height),
-      Search.diagonal_scan_right(flat, pattern, width, height),
-      Search.diagonal_scan_right(flat_reversed, pattern, width, height),
+    
+    slices = [
+      Search.horizontal_slices(flat, width),
+      Search.vertical_slices(flat, width, height),
+      Search.diagonal_slices_left(flat, width, height),
+      Search.diagonal_slices_right(flat, width, height),
     ] |> Enum.concat()
+    reversed = slices
+
+    |> Enum.map(&String.reverse/1)
+    
+    [slices, reversed] |> Enum.concat
   end
   
-  def vertical_scan(flat_grid, pattern, offset, height) do
+  def vertical_slices(flat_grid, width, height) do
     results = 0..height-1
     |> Enum.map(fn column -> 
-      Enum.slice(flat_grid, column..(offset*height)-(column-1)//height)
+      Enum.slice(flat_grid, column..(width*height)//width)
       |> Enum.join("")
     end)
-    |> Enum.map(fn slice -> Regex.scan(pattern, slice) |> Enum.concat end)
-    |> Enum.concat
   end
 
-  def horizontal_scan(flat_grid, pattern, width) do
-    results = flat_grid
+  def horizontal_slices(flat_grid, width) do
+    slices = flat_grid
     |> Enum.chunk_every(width)
     |> Enum.map(fn slice -> Enum.join(slice, "") end)
-    |> Enum.map(fn slice -> Regex.scan(pattern, slice) |> Enum.concat end)
-    |> Enum.concat
   end
   
-  def diagonal_scan_left(flat_grid, pattern, width, height) do
+  def diagonal_slices_left(flat_grid, width, height) do
     vertical_starts = 0..width-1
     horizontal_starts = (width*2)-1..(width*height)-1//height
-    results = Enum.concat([vertical_starts, horizontal_starts])
+    slices = Enum.concat([vertical_starts, horizontal_starts])
     |> Enum.map(fn start -> 
       slice = Enum.slice(flat_grid, start..(width*start)//width-1)
       |> Enum.join("")
     end) 
-    |> Enum.map(fn slice -> Regex.scan(pattern, slice) |> Enum.concat end) 
-    |> Enum.concat
   end
   
-  def diagonal_scan_right(flat_grid, pattern, width, height) do
+  def diagonal_slices_right(flat_grid, width, height) do
     vertical_starts = width-1..0 
     |> Enum.with_index
     horizontal_starts = width..width*(height-1)//height 
     |> Enum.reverse
     |> Enum.with_index
-    IO.inspect(Enum.concat([vertical_starts, horizontal_starts]), limit: :infinity)
      
-    results = Enum.concat([vertical_starts, horizontal_starts])
+    slices = Enum.concat([vertical_starts, horizontal_starts])
     |> Enum.map(fn {start, idx} -> 
       slice = Enum.slice(flat_grid, start..start+(width*(idx+1))//width+1)
       |> Enum.join("")
     end) 
-    |> Enum.map(fn slice -> Regex.scan(pattern, slice) |> Enum.concat end) 
-    |> Enum.concat
   end
 end
 
 {:ok, input} = File.read("../input/04-input.txt")
 
-occurances = Search.scan_multi(input, ~r/XMAS/) 
+occurances = Search.all_slices(input) 
+|> Enum.map(fn slice -> Regex.scan(~r/XMAS/, slice) |> Enum.concat end) 
+|> Enum.concat
 |> Enum.count()
+IO.inspect(occurances, charlists: :as_lists)
 IO.puts("Part 1 result #{occurances}")
-
